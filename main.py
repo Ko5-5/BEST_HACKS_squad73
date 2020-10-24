5cd6f6aa1ad5d4eec2e3c93d8f01dc6d9a661164
Michał Kos <58396234+Ko5-5@users.noreply.github.com>
2020-10-24 18:43:44 +0200

    commit

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import NumericProperty,ListProperty, ReferenceListProperty, ObjectProperty, BooleanProperty, StringProperty
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+from kivy.clock import Clock
+from random import randrange
+from kivy.core.window import Window
+from kivy.uix.textinput import TextInput
 
 ##class Button(Screen):
 
 
-class StartScreen(Widget):
+class StartScreen(Screen):
     def Przycisk(self):
-        button = Button(text = 'Listy zakupów', size_hint=(0.5,0.5), font_size='20sp', pos_hint={'center_x':0.1 , 'center_y':0.1})
-        return button
-
-
-class TwojaLodowaApp(App):
-    def build(self):
         layout = GridLayout(cols=3, row_force_default=True, row_default_height=60)
         #btn1 = Button(text = 'Listy zakupów', size_hint=(0.5,0.5), font_size='20sp', pos_hint={'center_x':0.1 , 'center_y':0.1})
         #btn2 = Button(text = 'Moja Ladówa', size_hint=(0.5,0.5), font_size='20sp', pos_hint={'center_x':0.1 , 'center_y':0.1})
@@ -26,6 +28,12 @@ class TwojaLodowaApp(App):
         layout.add_widget(btn2)
         layout.add_widget(btn3)
         return layout
+
+
+class TwojaLodowaApp(App):
+    def build(self):
+        apka = StartScreen()
+        return apka
 
 
 
