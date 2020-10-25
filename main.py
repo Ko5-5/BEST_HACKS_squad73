5350aaeaa4a181a912119de6b97c8d6c393b23ec
Michał Kos <58396234+Ko5-5@users.noreply.github.com>
2020-10-25 02:58:57 +0200

    listy przepisow

--- main.py
+++ main.py
@@ -22,9 +22,9 @@ from kivy.uix.label import Label
 import time
 ##class Button(Screen):
 
-class RV(RecycleView):
+class RV_Lodowa(RecycleView):
     def __init__(self,**kwargs):
-        super(RV,self).__init__(**kwargs)
+        super(RV_Lodowa,self).__init__(**kwargs)
         self.event = Clock.schedule_interval(self.read, 1.0/10.0)
         self.time_start = time.time()
 
@@ -53,26 +53,76 @@ class RV2(RecycleView):
         
         self.data = [{'text':str(x)} for x in lista_zak]
 
+class RV_Sniadanie(RecycleView):
+    def __init__(self,**kwargs):
+        super(RV_Sniadanie,self).__init__(**kwargs)
+        
+        self.event = Clock.schedule_interval(self.read, 1.0/10.0)
+        self.time_start = time.time()
+
+    def read(self, dt):
+        f = open("sniadanie_przepis.txt", "r")
+        lista_zak = list(())
+        for x in f:
+            lista_zak.append(x)
+        f.close()
+        
+        self.data = [{'text':str(x)} for x in lista_zak]
+
+class RV_Lunch(RecycleView):
+    def __init__(self,**kwargs):
+        super(RV_Lunch,self).__init__(**kwargs)
+        
+        self.event = Clock.schedule_interval(self.read, 1.0/10.0)
+        self.time_start = time.time()
+
+    def read(self, dt):
+        f = open("lunch_przepis.txt", "r")
+        lista_zak = list(())
+        for x in f:
+            lista_zak.append(x)
+        f.close()
+        
+        self.data = [{'text':str(x)} for x in lista_zak]
+
+class RV_Obiad(RecycleView):
+    def __init__(self,**kwargs):
+        super(RV_Obiad,self).__init__(**kwargs)
+        
+        self.event = Clock.schedule_interval(self.read, 1.0/10.0)
+        self.time_start = time.time()
+
+    def read(self, dt):
+        f = open("obiad_przepis.txt", "r")
+        lista_zak = list(())
+        for x in f:
+            lista_zak.append(x)
+        f.close()
+        
+        self.data = [{'text':str(x)} for x in lista_zak]
+
+class RV_Kolacja(RecycleView):
+    def __init__(self,**kwargs):
+        super(RV_Kolacja,self).__init__(**kwargs)
+        
+        self.event = Clock.schedule_interval(self.read, 1.0/10.0)
+        self.time_start = time.time()
+
+    def read(self, dt):
+        f = open("kolacja_przepis.txt", "r")
+        lista_zak = list(())
+        for x in f:
+            lista_zak.append(x)
+        f.close()
+        
+        self.data = [{'text':str(x)} for x in lista_zak]
+
 class ScreenManagement(ScreenManager):
     def __init__(self, **kwargs):
         super(ScreenManagement, self).__init__(**kwargs)
 
 class StartScreen(Screen):
-    def __init__(self, **kwargs):
-        super(StartScreen, self).__init__(**kwargs)
-
-    def Przycisk(self):
-        layout = GridLayout(cols=3, row_force_default=True, row_default_height=60)
-        #btn1 = Button(text = 'Listy zakupów', size_hint=(0.5,0.5), font_size='20sp', pos_hint={'center_x':0.1 , 'center_y':0.1})
-        #btn2 = Button(text = 'Moja Ladówa', size_hint=(0.5,0.5), font_size='20sp', pos_hint={'center_x':0.1 , 'center_y':0.1})
-        #btn3 = Button(text = 'Przepisy', size_hint=(0.5,0.5), font_size='20sp', pos_hint={'center_x':0.1 , 'center_y':0.1})
-        btn1 = Button(text = 'Listy zakupów')
-        btn2 = Button(text = 'Moja Ladówa')
-        btn3 = Button(text = 'Przepisy')
-        layout.add_widget(btn1)
-        layout.add_widget(btn2)
-        layout.add_widget(btn3)
-        return layout
+    pass
 
 class ListyZakupow(Screen):
     shop_list = ListProperty()
@@ -118,8 +168,17 @@ class MojaLodowa(Screen):
 class Przepisy(Screen):
     pass
 
+class Sniadanie(Screen):
+    pass
 
+class Lunch(Screen):
+    pass
 
+class Obiad(Screen):
+    pass
+
+class Kolacja(Screen):
+    pass
 
 class TwojaLodowaApp(App):
     def build(self):
